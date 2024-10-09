0b67ce5a0122903465758e9ba4a20879e8cc8345
Sanjay Khatri <48218848+sanju9891@users.noreply.github.com>
2024-10-09 11:11:21 +0530

    spelling update

--- statuscheck.sh
+++ statuscheck.sh
@@ -3,7 +3,7 @@ echo " Welcome to service status check script "
 read -p "Enter the service name to check its status: " service_name
 if [ -z $service_name ];
 then
-	echo " Please eneter a valid service name "
+	echo " Please enter a valid service name "
 else
 	systemctl status $service_name
 fi
